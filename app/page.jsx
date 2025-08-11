f1d6080acc7716ce85a5e9adb94c0bfc0a3b3306
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-08-12 00:35:02 +0100

    Import statement added to home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,11 @@
-"use client";
+'use client'
+
+import { useEffect, useState } from 'react'
+import { fetchTopCoins } from '../lib/coingecko'
+import PriceTicker from '../components/PriceTicker'
+import CryptoChart from '../components/CryptoChart'
+import CurrencyConverter from '../components/CurrencyConverter'
 
 export default function HomePage() {
-  return (
-    <div>Homepage</div>
-  );
+  return <div>Homepage</div>
 }
