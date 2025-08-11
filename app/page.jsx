120193808407b801d613d32aa4a3c7ae8368f195
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-08-12 00:39:40 +0100

    constants declared

--- app/page.jsx
+++ app/page.jsx
@@ -6,6 +6,11 @@ import PriceTicker from '../components/PriceTicker'
 import CryptoChart from '../components/CryptoChart'
 import CurrencyConverter from '../components/CurrencyConverter'
 
+const [coins, setCoins] = useState([])
+const [selectedCoinId, setSelectedCoinId] = useState('bitcoin')
+const [loadingCoins, setLoadingCoins] = useState(true)
+const REFRESH_MS = 60000 // 60s
+
 export default function HomePage() {
   return <div>Homepage</div>
 }
